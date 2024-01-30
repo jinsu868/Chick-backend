51ade8173cb7ee344af4d61f332d710ac8cc1656
jinsu868 <mjinsu098@gmail.com>
2024-01-30 21:22:33 +0900

    project init

---
+++ src/test/java/story/cheek/CheekApplicationTests.java
@@ -0,0 +1,13 @@
+package story.cheek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
