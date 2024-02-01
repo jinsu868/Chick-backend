2c88ccbb97875c5448e9b5775b27596b98acd6da
yoojaeyoonGit <jaeyoon321@naver.com>
2024-02-01 19:49:28 +0900

    feat: security 관련 설정

---
+++ src/main/java/story/cheek/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package story.cheek.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import story.cheek.security.*;
+import story.cheek.security.oauth2.OAuth2AuthenticationFailureHandler;
+import story.cheek.security.oauth2.OAuth2AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private static final String ADMIN = "ADMIN";
+    private final TokenProvider tokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieAuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptionHandlingConfigurer -> {
+                    exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+                    exceptionHandlingConfigurer.accessDeniedHandler(jwtAccessDeniedHandler);
+                })
+                .headers(httpSecurityHeadersConfigurer ->
+                        httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .sessionManagement(SessionManagementConfigurer ->
+                        SessionManagementConfigurer.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry ->
+                                authorizationManagerRequestMatcherRegistry
+                                        .requestMatchers("/").permitAll()
+                                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oAuth2LoginConfigurer -> {
+                    oAuth2LoginConfigurer.authorizationEndpoint(authorizationEndpointConfig -> {
+                        authorizationEndpointConfig.baseUri("/api/oauth2/authorize");
+                        authorizationEndpointConfig.authorizationRequestRepository(cookieAuthorizationRequestRepository());
+                    });
+                    oAuth2LoginConfigurer.redirectionEndpoint(redirectionEndpointConfig ->
+                            redirectionEndpointConfig.baseUri("/api/oauth2/callback/*")
+                    );
+                    oAuth2LoginConfigurer.userInfoEndpoint(userInfoEndpointConfig ->
+                            userInfoEndpointConfig.userService(customOAuth2UserService)
+                    );
+                    oAuth2LoginConfigurer.successHandler(oAuth2AuthenticationSuccessHandler);
+                    oAuth2LoginConfigurer.failureHandler(oAuth2AuthenticationFailureHandler);
+                })
+                .addFilterBefore(new TokenAuthenticationFilter(tokenProvider, customUserDetailsService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
