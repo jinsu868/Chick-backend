fb3f0c6f3c7cbc33997c9d128197c99c810f48af
jinsu868 <mjinsu098@gmail.com>
2024-02-05 16:27:55 +0900

    feat: 질문 엔티티 연관관계 세팅 (#3)

--- src/main/java/story/cheek/question/Question.java
+++ src/main/java/story/cheek/question/Question.java
@@ -4,13 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import story.cheek.common.domain.BaseEntity;
+import story.cheek.member.domain.Member;
 
 @Entity
 @Getter
@@ -27,4 +31,10 @@ public class Question extends BaseEntity {
 
     @Column(nullable = false)
     private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
