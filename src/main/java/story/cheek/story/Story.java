3d9fc406f574b41428074307ee3b3e40ae6a24a7
jinsu868 <mjinsu098@gmail.com>
2024-02-05 22:40:09 +0900

    feat: Story 엔티티 뼈대 작성 (#5)

---
+++ src/main/java/story/cheek/story/Story.java
@@ -0,0 +1,44 @@
+package story.cheek.story;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import story.cheek.common.domain.BaseEntity;
+import story.cheek.member.domain.Member;
+import story.cheek.question.domain.Occupation;
+import story.cheek.question.domain.Question;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Story extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Occupation occupation;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+}
