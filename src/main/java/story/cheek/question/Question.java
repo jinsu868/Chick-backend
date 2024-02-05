7d6880a642c1b1fc1a6d7151bcfb710e290c7e0b
jinsu868 <mjinsu098@gmail.com>
2024-02-05 16:13:59 +0900

    feat: 질문 엔티티 뼈대 작성 (#2)

---
+++ src/main/java/story/cheek/question/Question.java
@@ -0,0 +1,29 @@
+package story.cheek.question;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Occupation occupation;
+
+    @Column(nullable = false)
+    private String title;
+}
