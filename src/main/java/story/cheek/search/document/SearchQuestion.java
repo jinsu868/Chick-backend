0c587ea8cb352100c1b2608dd6389cf5e4dba182
yoojaeyoonGit <jaeyoon321@naver.com>
2024-02-20 19:48:55 +0900

    feat: 검색용 ES 설정

---
+++ src/main/java/story/cheek/search/document/SearchQuestion.java
@@ -0,0 +1,31 @@
+package story.cheek.search.document;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.*;
+import story.cheek.question.domain.Occupation;
+import story.cheek.search.EsIndex;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Setting(settingPath = "/static/elastic/settings.json")
+@Mapping(mappingPath = "/static/elastic/mappings.json")
+@Document(indexName = EsIndex.QUESTION_INDEX)
+public class SearchQuestion {
+    @Id
+    @Field(name = "question_id", type = FieldType.Long)
+    private Long questionId;
+
+    @Field(name = "occupation", type = FieldType.Text)
+    private Occupation occupation;
+
+    @Field(name = "title", type = FieldType.Text)
+    private String title;
+
+    @Field(name = "content", type = FieldType.Text)
+    private String content;
+}
