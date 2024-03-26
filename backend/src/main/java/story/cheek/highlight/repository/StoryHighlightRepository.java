f39878e3145709851f1a3a38c0f61b7cd79482fb
jinsu868 <mjinsu098@gmail.com>
2024-03-27 02:48:35 +0900

    refactor: 하이라이트 삭제 한방 쿼리로 변경 (#23)

--- backend/src/main/java/story/cheek/highlight/repository/StoryHighlightRepository.java
+++ backend/src/main/java/story/cheek/highlight/repository/StoryHighlightRepository.java
@@ -1,7 +1,11 @@
 package story.cheek.highlight.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import story.cheek.highlight.domain.Highlight;
 import story.cheek.highlight.domain.StoryHighlight;
 import story.cheek.story.domain.Story;
@@ -10,4 +14,11 @@ public interface StoryHighlightRepository extends JpaRepository<StoryHighlight, 
     Optional<StoryHighlight> findByHighlightAndStory(Highlight highlight, Story story);
 
     boolean existsByHighlightAndStory(Highlight highlight, Story story);
+
+    @Query("select sh from StoryHighlight sh where sh.highlight.id = :highlightId")
+    List<StoryHighlight> findAllByHighlightId(Long highlightId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("delete from StoryHighlight sh where sh.highlight.id = :highlightId")
+    void deleteAllByHighlightId(Long highlightId);
 }
